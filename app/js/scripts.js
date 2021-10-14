8218336b6b0ff2e792cacc753c2f88a2eccabd81
twilson-oc <twilson@outsource-consultants.com>
2021-10-14 13:28:10 -0600

    First commit son

---
+++ app/js/scripts.js
@@ -0,0 +1,624 @@
+var testingArray = [
+    "24-7 Intouch",
+    "A&T Ascension Consulting Inc",
+    "ACD Direct",
+    "AGR Group",
+    "ARC Management Group",
+    "ARCSmart Solutions",
+    "AW Companies Inc",
+    "Adentcom S de RL de CV",
+    "Advensus",
+    "Agents Republic",
+    "Akorbi",
+    "AlfredoTest",
+    "Alliance DMS Inc",
+    "Allied Customer Solutions",
+    "Allied Global",
+    "AmeeraTel",
+    "American Customer Care",
+    "American Help Desk",
+    "Ameridial Inc",
+    "Ansafone Contact Centers",
+    "AnswerNet Inc",
+    "Aptitude 360 Inc",
+    "Argo ITC",
+    "Axion Contact",
+    "Azpired",
+    "BDO Esnad",
+    "BPO Centers",
+    "Barrister Global Services Network Inc",
+    "Bellsystem24Hoasao",
+    "Bill Gosling Outsourcing",
+    "Blake and Associates Holdings (Pty) Ltd",
+    "Boomerang SA",
+    "Bosch",
+    "Business and Consumer Choices Telemarketing Ltd",
+    "Buwelo",
+    "CC Capital Group",
+    "CCB Credit Services",
+    "CCI Mauritius Limited",
+    "CCI Systems Inc",
+    "CCIG Group",
+    "CNM BPO Solutions",
+    "CSS Corp",
+    "CTC TeleServices",
+    "Calibrus Call Center Services LLC",
+    "California Marketing Group",
+    "Call 4 Health",
+    "Call Center Services International",
+    "Call Zilla",
+    "Caribbean Contact Center",
+    "Cati Call Center",
+    "Cebu Tele-net Philippines Corp",
+    "Centris",
+    "Cir Tech",
+    "Clear Nexus",
+    "CloudTask",
+    "Cloudstaff",
+    "CogentHub",
+    "Collective Solution International",
+    "Comdata Group UK Limited",
+    "Company Voice LLC",
+    "Contact Centers Dominicana",
+    "Contact Resolution Inc",
+    "Contact Source Solutions LLC",
+    "ContactPoint 360",
+    "ContactWorks",
+    "Contacto GCS",
+    "Conversion Calls LLC",
+    "Corporate Technologies Advantage",
+    "Costa Rica Call Center",
+    "Credit Solutions Services Pty Limited",
+    "Cuentame SA",
+    "Cuore Group",
+    "Customer Contact Services",
+    "Customer Direct",
+    "DG Solutions",
+    "Datamark Incorporation",
+    "DialAmerica Marketing Inc",
+    "Dialog One",
+    "Direct Interactions Inc",
+    "Direct Marketing Partners",
+    "Disabled Veterans Call Center",
+    "Distressed Assets S.A.",
+    "Dominion Outsourcing",
+    "Dynamics Marketing Inc",
+    "ECE Contact Centers",
+    "EGS Global, Inc.",
+    "EMERGIA CUSTOMER CARE COLOMBIA SAS",
+    "EMS Inc",
+    "Eagle One",
+    "Ecco Group USA",
+    "Elevation Connect LLC",
+    "Ellora Telecom Philippines Inc",
+    "Emerge BPO",
+    "Empire One Global Solutions",
+    "Endicott Contact Centers",
+    "Etech",
+    "Eteleconnect Inc",
+    "Euro Interactive International",
+    "Everise LLC",
+    "Expivia",
+    "FM Outsource",
+    "Feesible Solutions LLC",
+    "First Call Resolution LLC",
+    "Five Star Call Center",
+    "Focus Services",
+    "Fusion BPO Services",
+    "FutureNow Technologies Inc",
+    "GGA Solutions",
+    "GLOBAL OUTSOURCING SOLUTIONS",
+    "GMS Connect",
+    "Gandalf IT Services LLP",
+    "General Revenue Corporation",
+    "Global Contact Services",
+    "Global Innovation Services",
+    "Global One Strategies",
+    "Global Remote Services SRL",
+    "Global Response",
+    "Global Virtuoso",
+    "GlowTouch Technologies",
+    "Grand Solutions",
+    "Great Falls Marketing",
+    "Grindstone Inc",
+    "Halloween Times",
+    "Hexaware Technologies Limited",
+    "Highlander Political Strategies",
+    "Humach",
+    "Human Call Centers LLC",
+    "IO Solutions Call Center Inc",
+    "ITM Marketing",
+    "Ideal Contact Center",
+    "Infinity",
+    "Infinity Brands",
+    "Infinity Support Group",
+    "InfoCision",
+    "Insight Communications",
+    "Invictus Solutions Limited",
+    "ItelBPO Solutions",
+    "J Teleconnections",
+    "J-Curve Technologies",
+    "Jindal Intellicom",
+    "KM2 Solutions",
+    "KocharTech",
+    "Konecta Argentina",
+    "Konecta Portugal",
+    "Konecta Spain",
+    "Landau Group",
+    "Leading Edge Connections",
+    "LinkActiv",
+    "Live Reps Call Center",
+    "LiveOps",
+    "MCI",
+    "Mango5",
+    "Marketing Alternatives Inc",
+    "MattsenKumar LLC",
+    "Mindpearl",
+    "Mount Healthcare Services",
+    "Mountain Valley Integrated Solutions",
+    "Multi Source Global Network Inc",
+    "Multiactivitati SRL",
+    "MySource LLC",
+    "NOVASORS",
+    "NexRep LLC",
+    "Next Door Solutions",
+    "OPSIFY",
+    "Office Beacon",
+    "OfficePartners360",
+    "OnQ Contact Center",
+    "One Contact Center Inc",
+    "OneLink Holdings SA",
+    "OneTouch Direct",
+    "Open Access BPO",
+    "Open Look",
+    "OptimumCX LLC",
+    "Options Marketing Group",
+    "Oregon Corrections Enterprises",
+    "OutPLEX",
+    "PERFORMATEL",
+    "Pena4 BPO",
+    "Perfect Pitch Technologies",
+    "PharmaCentra",
+    "PharmaKinnex Inc",
+    "Pondera Services LLC",
+    "Premier BPO",
+    "ProCom LLC",
+    "Promocall",
+    "Promoveo LLC",
+    "Protel",
+    "Proximo",
+    "Quality Contact Solutions Inc",
+    "Quality Customer Service & Sales Inc",
+    "RDI Corporation Inc",
+    "ROI Call Center Solutions",
+    "Radial",
+    "Radius Global Solutions",
+    "Raya Contact Center",
+    "Regalix Inc",
+    "Revenue Group",
+    "SRG Asia Pacific Sdn B",
+    "Segoso Caribbean",
+    "Sequence Health",
+    "Sibling Solutions",
+    "Simetrix Solutions LLC",
+    "Simply Contact Ltd",
+    "SinglePoint Group International",
+    "SkyCom Call Center",
+    "Skybridge Americas Inc",
+    "Smart Connect Outsourcing Solutions",
+    "Solvo Global",
+    "Stafford Communications",
+    "Stateside BPO",
+    "Support Services Group Call Center",
+    "TK Enterprises Inc",
+    "TMP Direct Inc",
+    "TSD Global",
+    "TalentWorldGroup Plc",
+    "Talk Americas",
+    "Tech Americas USA",
+    "TechMahindra",
+    "Techcel Outsourcing LLC",
+    "TeleSpeak",
+    "Telecontacto",
+    "Telepoint Communications",
+    "Tell2Tell",
+    "The Connection",
+    "The Heller Group",
+    "The Office Gurus",
+    "The Stride Center",
+    "Transparent BPO",
+    "Tri Source International",
+    "Tyme Global Technologies",
+    "UAssistMe",
+    "US4 IT SUPPORT LLC",
+    "Ujet Inc",
+    "Underguard Teleservices",
+    "Unido Digital",
+    "Unifin, Inc.",
+    "United Language Group",
+    "United Nearshore Operations",
+    "Universal Portfolio LLC",
+    "VANAD CONTACT CENTERS NEDERLAND BV",
+    "VSA Inc",
+    "Ver A Fast",
+    "Verticalops",
+    "Virtual Staffing",
+    "Visaya KPO",
+    "VivaCALL",
+    "Voysus Connection Experts Inc",
+    "Vsynergize Outsourcing Pvt Ltd",
+    "WE Marketing",
+    "Working Access Solutions Inc",
+    "Working Solutions",
+    "World Connection",
+    "Zero Variance",
+    "active contact",
+    "iCall Services Inc",
+    "iContact BPO",
+    "sds"
+]
+
+
+
+
+
+var moduleName = "";
+var recordId = "";
+var dealName = "";
+var allRecordData = {};
+var pageNumber = 1;
+var partnerNameArray = [];
+var selectedPartnersArray = [];
+var sAArray = [];
+var sBSArray = [];
+var cCSArray = [];
+var sVSFArray = [];
+var cAArray = [];
+var initialArray = []
+
+
+//################################################
+//Testing Version Below
+//################################################
+
+createPartnerTable();
+
+
+function createPartnerTable() {
+    testingArray.sort();
+    var resTableBody = document.getElementById("responseTableBody");
+    for (let i = 0; i < testingArray.length; i++){
+        var newRow = document.createElement("TR");
+        for(let x = 0; x < 5; x++){
+            var newCol = document.createElement("TH");
+            newCol.setAttribute("class","partnerColNum" + x);
+            newCol.innerHTML = testingArray[i];
+            newRow.appendChild(newCol);
+            resTableBody.appendChild(newRow);
+        }
+    }
+    console.log(resTableBody);
+    var partnerList = [].slice.call(document.querySelectorAll('#responseTableBody TH'))
+    partnerList.forEach(function (triggerEl) {
+        var tabTrigger = new bootstrap.Tab(triggerEl)
+        triggerEl.addEventListener('click', function (event) {
+            var row = $(this).parents("tr:first")
+            console.log(row)
+            event.preventDefault()
+            tabTrigger._element.classList.toggle("partner-selected")
+            var picklistClass = tabTrigger._element.classList[0]
+            var picklistSelectedClass = tabTrigger._element.classList[1]
+            var partnerText = tabTrigger._element.textContent
+            if(picklistSelectedClass == "partner-selected")
+            {
+                if(picklistClass == "partnerColNum0")
+                {
+                    sAArray.push(partnerText)
+                    console.log(sAArray)
+                    console.log(sAArray.un)
+                }
+                else if(picklistClass == "partnerColNum1")
+                {
+                    sBSArray.push(partnerText)
+                }
+                else if(picklistClass == "partnerColNum2")
+                {
+                    cCSArray.push(partnerText)
+                }
+                else if(picklistClass == "partnerColNum3")
+                {
+                    sVSFArray.push(partnerText)
+                }
+                else if(picklistClass == "partnerColNum4")
+                {
+                    cAArray.push(partnerText)
+                }
+                else
+                {
+                    console.log("Choice Not Caught????")
+                }
+            }
+            else
+            {
+                if(picklistClass == "partnerColNum0")
+                {
+                    var filteredElements = sAArray.filter(val => (val != partnerText))
+                    sAArray = filteredElements
+                }
+                else if(picklistClass == "partnerColNum1")
+                {
+                    var filteredElements = sBSArray.filter(val => (val != partnerText))
+                    sBSArray = filteredElements
+                }
+                else if(picklistClass == "partnerColNum2")
+                {
+                    var filteredElements = cCSArray.filter(val => (val != partnerText))
+                    cCSArray = filteredElements
+                }
+                else if(picklistClass == "partnerColNum3")
+                {
+                    var filteredElements = sVSFArray.filter(val => (val != partnerText))
+                    sVSFArray = filteredElements
+                }
+                else if(picklistClass == "partnerColNum4")
+                {
+                    var filteredElements = cAArray.filter(val => (val != partnerText))
+                    cAArray = filteredElements
+                }
+                else
+                {
+                    console.log("Choice Not Caught????")
+                }
+            }
+        })
+    })
+}
+
+
+
+$(document).ready(function(){
+    $("#tableSearch").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#responseTableBody th").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//################################################
+//Prod Version Below
+//################################################
+/*
+ZOHO.embeddedApp.on("PageLoad", entity => {
+    var moduleName = entity.Entity;
+    var recordId = entity.EntityId; 
+    ZOHO.CRM.API.getRecord({Entity: moduleName, RecordID: recordId})
+    .then(function (recordData) {
+        recordData = recordData.data[0];
+        dealName = recordData.Deal_Name;
+        document.getElementById("oppId").value = recordData.id;
+        allRecordData = recordData;
+        getPartners();
+        console.log(recordData)
+    });    
+});
+ZOHO.embeddedApp.init();
+
+
+
+function getPartners() {
+    ZOHO.CRM.API.searchRecord({Entity:"Vendors",Type:"criteria",Query:"(Status:equals:CCCA Executed)",per_page:200,page:pageNumber})
+    .then(function(response){
+        var allPartnerRecs = response.data;
+        allPartnerRecs.forEach(obj => {
+            partnerNameArray.push(obj.Vendor_Name);
+        });
+        if(response.info.more_records == true)
+        {
+            pageNumber += 1;
+            return getPartners();
+        }
+        else
+        {
+            createPartnerTable();
+            console.log(partnerNameArray);
+        }
+    });
+};
+
+
+
+
+
+function createPartnerTable() {
+    partnerNameArray.sort();
+    var resTableBody = document.getElementById("responseTableBody");
+    for (let i = 0; i < partnerNameArray.length; i++){
+        var newRow = document.createElement("TR");
+        var newCol = document.createElement("TH");
+        newCol.setAttribute("class","partnerColNum");
+        newCol.innerHTML = partnerNameArray[i];
+        newRow.appendChild(newCol);
+        resTableBody.appendChild(newRow);
+    }
+    var partnerList = [].slice.call(document.querySelectorAll('#responseTableBody TH'))
+    partnerList.forEach(function (triggerEl) {
+        var tabTrigger = new bootstrap.Tab(triggerEl)
+        triggerEl.addEventListener('click', function (event) {
+            event.preventDefault()
+            tabTrigger._element.classList.toggle("partner-selected")
+        })
+    })
+}
+
+
+function createPartnerTable() {
+    partnerNameArray.sort();
+    console.log(partnerNameArray)
+    
+    var resTableBody = document.getElementById("responseTableBody");
+    for (let i = 0; i < partnerNameArray.length; i++){
+        var newRow = document.createElement("TR");
+        for(let x = 0; x < 5; x++){
+            var newCol = document.createElement("TH");
+            newCol.setAttribute("class","partnerColNum" + x);
+            newCol.innerHTML = partnerNameArray[i];
+            newRow.appendChild(newCol);
+            resTableBody.appendChild(newRow);
+        }
+    }
+    console.log(resTableBody);
+    var partnerList = [].slice.call(document.querySelectorAll('#responseTableBody TH'))
+    partnerList.forEach(function (triggerEl) {
+        var tabTrigger = new bootstrap.Tab(triggerEl)
+        triggerEl.addEventListener('click', function (event) {
+            event.preventDefault()
+            tabTrigger._element.classList.toggle("partner-selected")
+            var picklistClass = tabTrigger._element.classList[0]
+            var picklistSelectedClass = tabTrigger._element.classList[1]
+            var partnerText = tabTrigger._element.textContent
+            if(picklistSelectedClass == "partner-selected")
+            {
+                if(picklistClass == "partnerColNum0")
+                {
+                    sAArray.push(partnerText)
+                }
+                else if(picklistClass == "partnerColNum1")
+                {
+                    sBSArray.push(partnerText)
+                }
+                else if(picklistClass == "partnerColNum2")
+                {
+                    cCSArray.push(partnerText)
+                }
+                else if(picklistClass == "partnerColNum3")
+                {
+                    sVSFArray.push(partnerText)
+                }
+                else if(picklistClass == "partnerColNum4")
+                {
+                    cAArray.push(partnerText)
+                }
+                else
+                {
+                    console.log("Choice Not Caught????")
+                }
+            }
+            else
+            {
+                if(picklistClass == "partnerColNum0")
+                {
+                    var filteredElements = sAArray.filter(val => (val != partnerText))
+                    sAArray = filteredElements
+                }
+                else if(picklistClass == "partnerColNum1")
+                {
+                    var filteredElements = sBSArray.filter(val => (val != partnerText))
+                    sBSArray = filteredElements
+                }
+                else if(picklistClass == "partnerColNum2")
+                {
+                    var filteredElements = cCSArray.filter(val => (val != partnerText))
+                    cCSArray = filteredElements
+                }
+                else if(picklistClass == "partnerColNum3")
+                {
+                    var filteredElements = sVSFArray.filter(val => (val != partnerText))
+                    sVSFArray = filteredElements
+                }
+                else if(picklistClass == "partnerColNum4")
+                {
+                    var filteredElements = cAArray.filter(val => (val != partnerText))
+                    cAArray = filteredElements
+                }
+                else
+                {
+                    console.log("Choice Not Caught????")
+                }
+            }
+        })
+    })
+}
+
+var triggerTabList = [].slice.call(document.querySelectorAll('#partnerButtons a'))
+triggerTabList.forEach(function (triggerEl) {
+    var tabTrigger = new bootstrap.Tab(triggerEl)
+    triggerEl.addEventListener('click', function (event) {
+        event.preventDefault()
+        tabTrigger._element.classList.toggle("partner-button")
+    })
+})
+
+function updatePartnerPicklist() {
+    //var recId = document.getElementById("oppId").value
+    partnersArray = [];
+    var config={
+        Entity:"Deals",
+        APIData:{
+            "id" : "1190034000100993270"
+        }
+    }
+    var activePartners = [].slice.call(document.querySelectorAll(".partner-selected"))
+    activePartners.forEach(function (partnerEl) {
+        partnersArray.push(partnerEl.innerHTML)
+    })
+    var activePartnerPicklist = [].slice.call(document.querySelectorAll(".partner-button"))
+    activePartnerPicklist.forEach(function (picklistEl) {
+        var picklistItem = picklistEl.innerHTML
+        if(picklistItem == "Sales Approved Partners")
+        {
+            config.APIData.Sales_Approved_Partners = partnersArray
+        }
+        else if(picklistItem == "SBS Selected Partners")
+        {
+            config.APIData.Partner_Search_Results = partnersArray
+        }
+        else if(picklistItem == "Conference Call Selected Partners")
+        {
+            config.APIData.Conference_Call_Selected_Partners = partnersArray
+        }
+        else if(picklistItem == "Site Visit Selected Finalist")
+        {
+            config.APIData.Client_Selected_Partner = partnersArray
+        }
+        else if(picklistItem == "Client Approved Partner")
+        {
+            config.APIData.Correct_Client_Approved_Vendors = partnersArray
+        }
+        else
+        {
+        }
+    })
+    ZOHO.CRM.API.updateRecord(config)
+    .then(function(data){
+        console.log(data)
+    });
+}
+
+
+$(document).ready(function(){
+    $("#tableSearch").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#responseTableBody th").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+});
+*/
+
